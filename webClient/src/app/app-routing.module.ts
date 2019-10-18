a47b483584d60e8cf497e5d86992560588a1d3cf
Mohammed Naim Guerroui <mohammed-naim.guerroui@etu.u-pec.fr>
2019-10-18 16:01:08 +0200

    init project structure

--- webClient/src/app/app-routing.module.ts
+++ webClient/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { AuthGuardService } from './core/auth/auth-guard.service';
+import { ProfileComponent } from './modules/profile/profile.component';
+import { HelpComponent } from './modules/help/help.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, canActivate: [AuthGuardService]},
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService]},
+  {path: 'settings', component: ProfileComponent, canActivate: [AuthGuardService]},
+  {path: 'help', component: HelpComponent, canActivate: [AuthGuardService]}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- webClient/src/app/app.module.ts
+++ webClient/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
+import { ProfileComponent } from './modules/profile/profile.component';
+import { SettingsComponent } from './modules/settings/settings.component';
+import { HelpComponent } from './modules/help/help.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    SettingsComponent,
+    HelpComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
