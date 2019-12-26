a09c0bc84c7c6065b59586a041ee7a36cfb3d26d
Anass <anasslakhloufi@gmail.com>
2019-12-26 03:11:35 +0100

    Login service

--- webClient/src/app/compenents/log-in/log-in.component.ts
+++ webClient/src/app/compenents/log-in/log-in.component.ts
@@ -4,6 +4,8 @@ import { LoginService } from '../../services/loginService/login.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 import { ForgetPasswordComponent } from './forget-password/forget-password.component'
+import { User } from 'src/app/entity/user';
+import { ResponsesCodes } from 'src/app/services/helpers/responsesCodesEnum';
 
 
 
@@ -14,11 +16,14 @@ import { ForgetPasswordComponent } from './forget-password/forget-password.compo
 })
 export class LogInComponent implements OnInit {
   loginForm: FormGroup;
+  user : User;
   submitted: boolean = false;
+  loginFailed : boolean = false;
+  serverError : boolean = false;
+  badRequest : boolean = false;
   bsModalRef: BsModalRef;
 
   constructor(private router: Router, private loginService: LoginService, private formBuilder: FormBuilder, private modalService: BsModalService) {
-
   }
 
   ngOnInit() {
@@ -35,30 +40,47 @@ export class LogInComponent implements OnInit {
   get getFormErrors() {
     return this.loginForm.controls; // to get access to errors in form
   }
+
   onSubmitForm() {
     this.submitted = true;
+    this.loginFailed = false;
+    this.serverError = false;
     if (this.loginForm.invalid) {
       return; //stop if the form is not valid
     }
     const formValue = this.loginForm.value;
     const email = formValue['email'];
     const password = formValue['password'];
+    this.user = new User(email, password);
 
-    let result: boolean = this.loginService.connect(email, password);
-
-    if (result == true) {
-      this.router.navigate(['/Home/Dashboard']);
-    } else {
-
-      //Print the Error 
-    }
-
+    this.loginService.connect(this.user).subscribe(
+      (response) => {
+        let json = JSON.parse(JSON.stringify(response));
+        let token = json.token;
+        let expireIn = 24;
+        //let expireIn = json.expireIn;
+        this.loginService.setTokenInfo(token,expireIn);
+        this.router.navigate(['/Home/Projects']);
+      },
+      (error) => {
+          if(error.error.error == ResponsesCodes.LOGIN_FAILED){
+            this.loginFailed = true;
+            this.serverError = false;
+            this.badRequest = false;
+          }else if(error.error.error == ResponsesCodes.SERVER_ERROR || error.status == 404){
+            this.loginFailed = false;
+            this.serverError = true;
+            this.badRequest = false;
+          }else if(error.error.error == ResponsesCodes.BAD_REQUEST){
+            this.badRequest = true;
+            this.loginFailed = false;
+            this.serverError = false;
+          }
+      })
   }
 
   openForgetPasswordPage() {
     this.bsModalRef = this.modalService.show(ForgetPasswordComponent, { class: 'modal-lg' });
     this.bsModalRef.content.closeBtnName = 'Close';
   }
-
-
 }
