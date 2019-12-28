0e38183926fc30b64a08c70334fe0400618b7ce1
Anass <anasslakhloufi@gmail.com>
2019-12-28 02:08:37 +0100

    Update models

--- webClient/src/app/entity/project.ts
+++ webClient/src/app/entity/project.ts
@@ -1,20 +1,50 @@
+import { User } from './user';
+import { Card } from './card';
 
 export class Project {
-    name: string;
-    priority:string;
+    id:number;
+    name: string;   
     description: string;
-    members:string[];
+    creationDate : number; //milliseconds 
+	updateDate : number; //milliseconds 
+    members: User[];
+    cards : Card[];
 
-
-
-    constructor(name:string,
-    priority: string,
-    description: string,
-    members:string[]) {
-        this.name = name;
-        this.priority=priority;
-        this.description=description;
-        this.members=members;
+    constructor(name:string,description: string,id?:number,creationDate?: number,updateDate?: number,members?:User[],cards?: Card[]) {
+        if(typeof name === "undefined" ||name === null){
+            this.name = "";
+        }else {
+            this.name = name;
+       }
+       if(typeof description === "undefined" ||description === null){
+            this.description = "";
+        }else {
+            this.description=description;
+        }
+        if(typeof id === "undefined" ||id === null){
+            this.id = null;
+        }else {
+            this.id=id;
+        }
+        if(typeof creationDate === "undefined" ||creationDate === null){
+            this.creationDate = null;
+        }else {
+            this.creationDate=creationDate;
+        }
+        if(typeof updateDate === "undefined" ||updateDate === null){
+            this.updateDate = null;
+        }else {
+            this.updateDate=updateDate;
+        }
+        if(typeof members === "undefined" ||members === null){
+            this.members = null;
+        }else {
+            this.members=members;
+        }
+        if(typeof cards === "undefined" ||cards === null){
+            this.cards = null;
+        }else {
+            this.cards=cards;
+        }
     }
-
 }
