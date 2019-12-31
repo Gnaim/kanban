ed2b5e59a8f97651302b373b16865b29575cebd8
Anass <anasslakhloufi@gmail.com>
2019-12-31 18:49:21 +0100

    Refactorig work componenent

--- webClient/src/app/compenents/home-page/work/tasks-list/tasks-list.component.ts
+++ webClient/src/app/compenents/home-page/work/tasks-list/tasks-list.component.ts
@@ -12,7 +12,6 @@ export class TasksListComponent implements OnInit {
   @Input('memberName') memberName : string;
   
   ngOnInit() {
-    console.log(this.card);
   }
   
 }
--- webClient/src/app/compenents/home-page/work/work.component.ts
+++ webClient/src/app/compenents/home-page/work/work.component.ts
@@ -14,13 +14,19 @@ import { MyNotificationsService } from 'src/app/services/notifications/notificat
 export class WorkComponent implements OnInit {
 
   bsModalRef: BsModalRef;
-  emptyResponse: boolean = false;
+  emptyResponse: boolean;
   projects : Project[];
+  isLoading: boolean;
+  errorGetTasks : boolean;
 
   constructor(private modalService: BsModalService,
-              private projectService: ProjectsService, private notification:MyNotificationsService) {}
+              private projectService: ProjectsService,
+              private notification:MyNotificationsService) {}
 
   ngOnInit() {
+    this.isLoading = true;
+    this.emptyResponse = false;
+    this.errorGetTasks = false;
     this.getTasksByProject();
   }
 
@@ -28,26 +34,25 @@ export class WorkComponent implements OnInit {
       this.projectService.getMyProjects().subscribe(
         (projects) =>{
           let jsonResponse = HttpHelpers.parseData(projects);
-          
           this.projects = jsonResponse.projects as Project[];
-          console.log(this.projects);
           let size = this.projects.length;
+
           if(size != 0){
             for(let i=0; i<size ;i++){
               if(this.projects[i].cards.length != 0){
                 this.emptyResponse = false;
-                return;
+                break;
               }
-              if(i == (size-1)){
-                this.emptyResponse = true;
-              }
+              if(i == (size-1)) this.emptyResponse = true;
             }
           }else{
             this.emptyResponse = true;
           }
+          this.isLoading = false;
         },
         (error) => {
-          this.notification.showErrorNotification(error);
+          this.errorGetTasks = this.notification.showErrorNotification(error);
+          this.isLoading = false;
         });
   }
 
