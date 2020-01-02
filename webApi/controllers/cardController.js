9546215e67878241eec9384e65ea8c2e9c439e0b
Abdelmalek <abdelmalek.benamirouche@cibiltech.com>
2020-01-02 17:26:14 +0100

    update cardController (populate members and createdBy) and verify that status and type are correct

--- webApi/controllers/cardController.js
+++ webApi/controllers/cardController.js
@@ -5,7 +5,8 @@ const users = mongoose.model('User');
 exports.getCardsByProject = (req, res, next) => {
   cards.find({ _project: req.params.id })
     .select('members title description status type checklist createdAt createdBy')
-    .populate('createBy', 'firstName lastName profession')
+    .populate({path:'members',select: 'firstName lastName profession email'})
+    .populate({path:'createdBy',select: 'firstName lastName profession email'})
     .exec((err, cards) => {
       if (err) {
         res.status(500).send({message:'There was a problem adding the information to the database.'});
@@ -24,62 +25,75 @@ exports.post = (req, res, next) => {
   const projectId = req.params.id;
   const title = req.body.title ? req.body.title: '';
   const description  = req.body.description ? req.body.description :'';
-  const status = req.body.status ? req.body.status: 'backlog';
-  const type = req.body.type ? req.body.type: 'dev';
+  const status = req.body.status ? req.body.status: 'Backlog';
+  const type = req.body.type ? req.body.type: 'Dev';
   const checklist = req.body.checklist ? req.body.checklist: [];
-  users.findOne({email:payload.data.email})
-  .exec((err,user) => {
-    if(err){
-      res.status(500).send({message: "server Error",
-                            error: 603});
+  const statuses= ['Backlog','Doing','Done'];
+  const types = ['Dev','Bug']
+  if (!types.includes(type))
+  {
+    res.status(400).send({message:"type must be Dev or Bug",
+                          error: 610});
+  }else{
+    if(!statuses.includes(status)){
+      res.status(400).send({message:"status must be Backlog, Doing or Done",
+                          error: 610});
     }else{
-      const createdBy = user._id;
-      const newCard = {
-        _project: projectId,
-        title,
-        description,
-        status,
-        type,
-        checklist,
-        createdBy, 
-      };
-      if(title == null || description == null){
-        res.status(400).send({message:"both title and description are required to create a card",
-                              error: 610})
-      }
-      else{
-        cards.create(newCard, (err, card) => {
-          projects.findById(req.params.id)
-            .exec((err, project) => {
-              project.updateOne({ $push: { cards: card._id } }, (err) => {
-                if (err) {
-                  res.status(500).send({message:`GET Error: There was a problem retrieving: ${err}`});
-                } else {
-                  projects.findById(req.params.id)
-                  .exec((err, project) =>{
+      users.findOne({email:payload.data.email})
+      .exec((err,user) => {
+        if(err){
+          res.status(500).send({message: "server Error",
+                                error: 603});
+        }else{
+          const createdBy = user._id;
+          const newCard = {
+            _project: projectId,
+            title,
+            description,
+            status,
+            type,
+            checklist,
+            createdBy, 
+          };
+          if(title == null || description == null){
+            res.status(400).send({message:"both title and description are required to create a card",
+                                  error: 610})
+          }
+          else{
+            cards.create(newCard, (err, card) => {
+              projects.findById(req.params.id)
+                .exec((err, project) => {
+                  project.updateOne({ $push: { cards: card._id } }, (err) => {
                     if (err) {
                       res.status(500).send({message:`GET Error: There was a problem retrieving: ${err}`});
                     } else {
-                      res.status(200).format({
-                        json: () => {
-                          res.json(project);
+                      projects.findById(req.params.id)
+                      .exec((err, project) =>{
+                        if (err) {
+                          res.status(500).send({message:`GET Error: There was a problem retrieving: ${err}`});
+                        } else {
+                          res.status(200).format({
+                            json: () => {
+                              res.json(project);
+                            }
+                          });
                         }
                       });
                     }
                   });
-                }
-              });
+                });
             });
-        });
-      }
+          }
+        }
+      });
     }
-  });
+  }
 };
 
 exports.getById = (req, res, next) => {
   cards.findById(req.params.cardId)
     .populate('checklists')
-    .populate('createdBy', 'firstName lastName profession')
+    .populate('createdBy', 'firstName lastName profession email')
     .exec((err, cards) => {
       if (err) {
         res.status(500).send({message:'There was a problem adding the information to the database.'});
@@ -100,23 +114,35 @@ exports.UpdateCardById = (req, res, next) => {
   const description  = req.body.description ? req.body.description : '';
   const type  = req.body.type ? req.body.type: 'dev';
   const checklist = req.body.checklist ? req.body.checklist : [];
-
-  cards.update({_id: req.params.cardId}, {
-    title: title,
-    status: status,
-    description: description,
-    type: type,
-    members: members,
-    checklist: checklist,
-  }).exec((err,card)=>{
-    if (err) {
-      console.log(err);
-      res.status(500).send({message:`${err}`,
-                            error: 603});
-    } else {
-      res.status(200).send({message:'card updated successfully'});
+  const statuses= ['Backlog','Doing','Done'];
+  const types = ['Dev','Bug']
+  if (!types.includes(type))
+  {
+    res.status(400).send({message:"type must be Dev or Bug",
+                          error: 610});
+  }else{
+    if(!statuses.includes(status)){
+      res.status(400).send({message:"status must be Backlog, Doing or Done",
+                          error: 610});
+    }else{
+      cards.update({_id: req.params.cardId}, {
+        title: title,
+        status: status,
+        description: description,
+        type: type,
+        members: members,
+        checklist: checklist,
+      }).exec((err,card)=>{
+        if (err) {
+          console.log(err);
+          res.status(500).send({message:`${err}`,
+                                error: 603});
+        } else {
+          res.status(200).send({message:'card updated successfully'});
+        }
+      });
     }
-  });
+  }
 };
 
 exports.deleteCardById = (req, res, next) => {
