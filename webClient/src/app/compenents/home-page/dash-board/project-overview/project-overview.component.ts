a1b8b5b1fba9dcadf73fc0a635a3e819f5b85317
Anass <anasslakhloufi@gmail.com>
2020-01-05 12:46:28 +0100

    Confirmation pop up delete

--- webClient/src/app/compenents/home-page/dash-board/project-overview/project-overview.component.ts
+++ webClient/src/app/compenents/home-page/dash-board/project-overview/project-overview.component.ts
@@ -1,23 +1,39 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, TemplateRef } from '@angular/core';
 import { ProjectsService } from 'src/app/services/projectService/projects.service';
 import { MyNotificationsService } from 'src/app/services/notifications/notifications.service';
 import { EventEmitter } from '@angular/core';
 import { Project } from 'src/app/entity/Project';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap';
 @Component({
   selector: 'app-project-overview',
   templateUrl: './project-overview.component.html',
   styleUrls: ['./project-overview.component.scss']
 })
 export class ProjectOverviewComponent implements OnInit {
-
+  bsModalRef: BsModalRef;
   private projectId : string;
   @Input('project') project : Project;
   @Output('refreshData') getProjectsEvent = new EventEmitter();
   
-  constructor(private projectService: ProjectsService,private notification:MyNotificationsService) { }
+  constructor(private projectService: ProjectsService,
+              private modalService: BsModalService,
+              private notification:MyNotificationsService) { }
 
   ngOnInit() {
       this.projectId = this.project._id;
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.bsModalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+
+  confirm(): void {
+    this.deleteProject();
+    this.bsModalRef.hide();
+  }
+ 
+  decline(): void {
+    this.bsModalRef.hide();
   }
 
   deleteProject() {
