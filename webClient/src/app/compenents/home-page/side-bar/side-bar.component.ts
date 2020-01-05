1b92de06634cb8bf0d20e9b8ea84504e9c5c6eb4
Anass <anasslakhloufi@gmail.com>
2020-01-05 11:23:58 +0100

    LogOut (Only in front no back end call)

--- webClient/src/app/compenents/home-page/side-bar/side-bar.component.ts
+++ webClient/src/app/compenents/home-page/side-bar/side-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/loginService/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService,
+              private router: Router) { }
 
   ngOnInit() {
   }
 
+  logOut(){
+    this.loginService.removeToken();
+    this.router.navigate(['Login']);
+  }
 }
